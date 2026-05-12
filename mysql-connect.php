b472acbdc3ba89725a0bb4252615d84d9900486b
AkiraVee <lantisvaldecantos0@gmail.com>
2026-05-13 04:11:07 +0800

    fix: uncomment database connection variables for proper configuration

--- mysql-connect.php
+++ mysql-connect.php
@@ -1,13 +1,13 @@
 <?php 
-//$dbhost = "	sql208.yzz.me";
-//$dbuser = "yzzme_41901649"; 
-//$dbpass = "DqugW7sno8pL"; 
-//$dbname = "yzzme_41901649_ticketdb"; 
+$dbhost = "	sql208.yzz.me";
+$dbuser = "yzzme_41901649"; 
+$dbpass = "DqugW7sno8pL"; 
+$dbname = "yzzme_41901649_ticketdb"; 
 
-$dbhost = "localhost";
-$dbuser = "root"; 
-$dbpass = ""; 
-$dbname = "ticketdb"; 
+//$dbhost = "localhost";
+//$dbuser = "root"; 
+//$dbpass = ""; 
+//$dbname = "ticketdb"; 
 
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass); 
 mysqli_select_db($conn, $dbname) or die("Unable to select database");  
